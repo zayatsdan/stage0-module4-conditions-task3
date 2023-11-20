9527bc040c3e0368f3b4c3c507984e2005ccfc74
zayatsdan <36342659+zayatsdan@users.noreply.github.com>
2023-11-20 16:16:38 +0100

    Update SalaryCalculator.java

--- src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
+++ src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
@@ -2,5 +2,21 @@ package school.mjc.stage0.conditions.task3;
 
 public class SalaryCalculator {
     public void calculateSalary(int salary) {
+        int taxes;
+        if (salary<=10000&&salary>0){
+            taxes=15;
+            System.out.println((salary * (100 - taxes ))/100);
+        }
+        if (salary>10000&&salary<=20000) {
+            taxes = 18;
+            System.out.println((salary * (100 - taxes ))/100);
+        }
+        if (salary>20000) {
+            taxes = 20;
+            System.out.println((salary * (100 - taxes ))/100);
+        }
+        if (salary<=0){
+            System.out.println("Wrong input!");
+        }
     }
 }
